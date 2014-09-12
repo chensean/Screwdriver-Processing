caea0140adaba7fc5667c946565f7f9c1599e523
shenxuejin <xuejinshen@126.com>
2014-09-12 18:01:34 +0800

    基础版本

--- Screwdriver-Core/data_buffer.h
+++ Screwdriver-Core/data_buffer.h
@@ -9,16 +9,19 @@
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
 #include "tm_parameter.h"
+#include "sub_buffer.h"
 
 namespace TM
 {
 	class data_buffer
+		:private boost::noncopyable
 	{
 	public:
 		data_buffer(void);
 		~data_buffer(void);
 		void add_parameter(const tm_parameter_ptr& param,uint32_t start_idx);
-		void update_buffer(const std::vector<uint8_t>& buffer);
+		void add_sub_buffer(const sub_buffer_ptr& sub_buffer,uint32_t start_idx);
+		void update_buffer(const std::vector<uint8_t>& data);
 	private:
 		struct data_buffer_imp_t;
 		boost::shared_ptr<data_buffer_imp_t> imp_;
